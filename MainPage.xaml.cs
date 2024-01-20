2d811443284f42ec163a9ba104fc589b98913da5
DevChrisNguyen <dev.chrisnguyen@gmail.com>
2024-01-19 22:45:15 -0600

    Formatting now the same on Entry and Result RX's. Entries are now required before RX is converted by App. Clear button now operational and is set below Converted Rx Grid Display.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace RxConverter
+﻿using System.Threading.Tasks;
+using System.Xml;
+
+namespace RxConverter
 {
     public partial class MainPage : ContentPage
     {
@@ -13,14 +16,66 @@
 
         }
 
-        private void OnConvertClicked(object sender, EventArgs e)
+        private async void OnConvertClicked(object sender, EventArgs e)
+
         {
-            double rSphereInput = double.Parse(RightSphere.Text);
-            double rCylInput = double.Parse(RightCyl.Text);
-            int rAxisInput = int.Parse(RightAxis.Text);
-            double lSphereInput = double.Parse(LeftSphere.Text);
-            double lCylInput = double.Parse(LeftCyl.Text);
-            int lAxisInput = int.Parse(LeftAxis.Text);
+            double rSphereInput;
+            double rCylInput;
+            int rAxisInput;
+            double lSphereInput;
+            double lCylInput;
+            int lAxisInput;
+            
+            if (RightSphere.Text == String.Empty) {
+                await DisplayAlert("Alert", "Right Sphere Must be Entered! (Enter 0 for No SPH)", "Eye, eye Capt'n");
+                return;
+            }
+            else {
+                rSphereInput = double.Parse(RightSphere.Text);
+                }
+            if (RightCyl.Text == String.Empty)
+            {
+                await DisplayAlert("Alert", "Right Cyl was not Entered! (Enter 0 for No CYL)", "Eye, eye Capt'n");
+                return;
+            }
+            else { rCylInput = double.Parse(RightCyl.Text); }
+            if (RightAxis.Text == String.Empty)
+            {
+                await DisplayAlert("Alert", "Right Axis is missing! (Enter 0 for No Axis)", "Eye, eye Capt'n");
+                return;
+            }
+            else {
+                rAxisInput = int.Parse(RightAxis.Text);
+            }
+            if (LeftSphere.Text == String.Empty) { 
+                await DisplayAlert("Alert", "Left Sphere Must be Entered.", "OK");
+            return;
+            }
+            else {
+                lSphereInput = double.Parse(LeftSphere.Text);
+    }
+            if (LeftCyl.Text == String.Empty)
+            {
+                await DisplayAlert("Alert", "Left Cyl Must be Entered. (Enter 0 for No Cyl", "OK");
+                return;
+            }
+            else { lCylInput = double.Parse(LeftCyl.Text); }
+            if (LeftAxis.Text == String.Empty)
+            {
+                await DisplayAlert("Alert", "You forgot the Left Axis! Try again.", "OK");
+                return;
+            }
+            else
+            {
+                lAxisInput = int.Parse(LeftAxis.Text);
+            }
+
+
+            rCylInput = double.Parse(RightCyl.Text);
+            rAxisInput = int.Parse(RightAxis.Text);
+            lSphereInput = double.Parse(LeftSphere.Text);
+            lCylInput = double.Parse(LeftCyl.Text);
+            lAxisInput = int.Parse(LeftAxis.Text);
 
             initialRx = new EyeRx(rSphereInput, rCylInput, rAxisInput, lSphereInput, lCylInput,lAxisInput );
 
@@ -37,16 +92,63 @@
 
         EyeRx ConvertCylinderNotation(EyeRx rx)
         {
-            double rSphere = rx.rightSphere + rx.rightCyl;
-            double rCyl = -1 * rx.rightCyl;
-            int rAxis = (rx.rightAxis + 90) % 180;
-            double lSphere = rx.leftSphere + rx.leftCyl;
-            double lCyl = -1 * rx.leftCyl;
-            int lAxis = (rx.leftAxis + 90) % 180;
+            double rSphere;
+            double rCyl;
+            int rAxis;
+            double lSphere;
+            double lCyl;
+            int lAxis;
+
+
+            rSphere = rx.rightSphere + rx.rightCyl;
+
+            if (rx.rightCyl == 0) {
+                rCyl = 0;
+            }
+            else
+            {
+                rCyl = -1 * rx.rightCyl;
+            }
+            
+            rAxis = (rx.rightAxis + 90) % 180;
+            if (rAxis == 0) {
+                rAxis = 180; 
+            }
+            lSphere = rx.leftSphere + rx.leftCyl;
+
+            if (rx.leftCyl == 0)
+            {
+                lCyl = 0;
+            }
+            else
+            {
+                lCyl = -1 * rx.leftCyl;
+            }
+
+            lAxis = (rx.leftAxis + 90) % 180;
+            if (lAxis == 0)
+            {
+                lAxis = 180;
+            }
             
             
 
             return new EyeRx(rSphere, rCyl, rAxis, lSphere, lCyl,lAxis);
+        }
+        private void OnClearClicked(Object sender, EventArgs e)
+        {
+            RightSphere.Text = string.Empty;
+            RightCyl.Text = string.Empty;
+            RightAxis.Text = string.Empty;
+            LeftSphere.Text = string.Empty;
+            LeftCyl.Text = string.Empty;
+            LeftAxis.Text = string.Empty;
+            ResultSphereR.Text = string.Empty;
+            ResultCylR.Text = string.Empty;
+            ResultAxisR.Text = string.Empty;
+            ResultSphereL.Text = string.Empty;
+            ResultCylL.Text = string.Empty;
+            ResultAxisL.Text = string.Empty;
         }
 
     }
